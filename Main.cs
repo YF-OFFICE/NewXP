6e7fd87578bd7f44c262bbfb49fbd44418014bbf
tyicu <tyicu>
2024-03-08 22:21:20 +0800

    更新文件

--- Main.cs
+++ Main.cs
@@ -1,6 +1,7 @@
 ﻿using Exiled.API.Features;
 using Exiled.API.Interfaces;
 using Exiled.Events.EventArgs.Player;
+using NewXp.IniApi;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,14 +23,31 @@ namespace NewXp
     {
         public override string Name { get; } = "NewXp";
         public override string Author { get; } = "tyIUc";
-        public static string dic  = Path.Combine(Exiled.API.Features.Paths.Configs, "NewXP")+"\\"; 
+        public static string dic  = Path.Combine(Exiled.API.Features.Paths.Configs, "NewXP")+ "\\Ssave.ini";
+        public static IniFile IniFilea { get; private set; } 
         public override Version Version { get; } = new Version(1, 0, 0);
-        public override void OnDisabled()
-        {
-            base.OnDisabled(); 
-        }
         public override void OnEnabled()
-        {     Log.Warn(dic); 
+        {    
+            Log.Warn("经验系统储存目录"+dic);
+            if (!Directory.Exists(dic.Replace("\\Ssave.ini", "")))
+            {
+                Directory.CreateDirectory(dic.Replace("\\Ssave.ini", ""));
+                Log.Warn("初始化");
+            
+            }
+            if (!File.Exists(dic))
+            { 
+               var test = new IniFile();
+            test.Section("XpSave").Set("test","test");
+            test.Section("LvSave").Set("test","test");
+            test.Save(dic);
+               
+            }
+           IniFilea = new IniFile(dic);
+         
+            Log.Info("插件存储连接成功");
+            
+
             base.OnEnabled();
         }
 
